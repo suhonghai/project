e319b9906ef8f815006b1022fa4d0ba836451da1
suhonghai <344593220@qq.com>
2018-03-08 18:59:25 +0800

    修改

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,20 @@ var gulp = require("gulp"),
 	sass = require("gulp-sass"),
 	connect = require("gulp-connect"),
 	htmlmin = require("gulp-htmlmin"),
-	babel = require("gulp-babel");
+	babel = require("gulp-babel"),
+	_root = "src";
 
 // 启动服务器
 gulp.task("server", function(){
 	connect.server({
-		root : "dist",
+		root : _root,
 		livereload : true
 	});
 });
 
 // 压缩JS
 gulp.task("js", function(){
-	gulp.src("js/**/*.js")
+	gulp.src("src/js/**/*.js")
 		.pipe(babel({
             presets: ['env']
         }))
@@ -26,7 +27,7 @@ gulp.task("js", function(){
 
 // 定义 gulp 任务，压缩 HTML
 gulp.task("html", function(){
-	gulp.src(["**/*.html", "!node_modules/**/*.html"])
+	gulp.src(["src/**/*.html"])
 		.pipe(htmlmin({collapseWhitespace: true, minifyCSS:true, minifyJS:true}))
 		.pipe(gulp.dest("dist"))
 		.pipe(connect.reload());
@@ -34,31 +35,31 @@ gulp.task("html", function(){
 
 // 定义任务，编译 sass
 gulp.task("sass", function(){
-	gulp.src("sass/**/*.scss")
+	gulp.src("src/sass/**/*.scss")
 		.pipe(sass({outputStyle:"compressed"}))
-		.pipe(gulp.dest("dist/css"))
+		.pipe(gulp.dest(_root + "/css"))
 		.pipe(connect.reload());
 });
 
 // 将图片、库、模拟的假数据复制到 dist 下
 gulp.task("images", function(){
-	gulp.src("images/**/*.*")
+	gulp.src("src/images/**/*.*")
 		.pipe(gulp.dest("dist/images"));
 });
 gulp.task("lib", function(){
-	gulp.src("lib/**/*.*")
+	gulp.src("src/lib/**/*.*")
 		.pipe(gulp.dest("dist/lib"));
 });
 gulp.task("mock", function(){
-	gulp.src("mock/**/*.*")
+	gulp.src("src/mock/**/*.*")
 		.pipe(gulp.dest("dist/mock"));
 });
 gulp.task("copyfile", ["images", "lib", "mock"]);
 
 gulp.task("watch", function(){
-	gulp.watch("sass/**/*.scss", ["sass"]);
-	gulp.watch("js/**/*.js", ["js"]);
-	// gulp.watch(["**/*.html"], ["html"]);
+	gulp.watch("src/sass/**/*.scss", ["sass"]);
+	gulp.watch("src/js/**/*.js", ["js"]);
+	gulp.watch(["src/**/*.html"], ["html"]);
 });
 
 gulp.task("default", ["html", "js", "sass", "copyfile", "server", "watch"]);
